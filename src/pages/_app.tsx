7a371a78cb2b423a8c1703086ec1761de78fea0e
arBalasquide <adrian.balasquide@gmail.com>
2021-03-08 11:23:25 -0400

    Implemented <Head> as intended

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import "../styles/styles.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍴</text></svg>"
+        />
+        <title>soyrecipes</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,11 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Main, Head, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            rel="icon"
-            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍴</text></svg>"
-          />
-          <title>soyrecipes</title>
-        </Head>
         <body>
+          <Head />
           <Main />
           <NextScript />
         </body>
