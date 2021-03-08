91a5de053d14baf0ba7f81b06ac799faa63f89fc
arBalasquide <adrian.balasquide@gmail.com>
2021-03-08 00:38:25 -0400

    Changed wording

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -11,9 +11,9 @@ const SubmitRecipe = () => {
         <a href="https://github.com/arBalasquide/soyrecipes#submitting-a-recipe-to-this-repo">
           this
         </a>{" "}
-        and follow the instructions for submitting a pull request. However, if
-        you do not posses a Github account nor the technical skills, you may
-        email your recipe at: ar AT balasquide DOT xyz
+        and follow the instructions for submitting a recipe. However, if you do
+        not posses a Github account nor the technical skills, you may email your
+        recipe at: ar AT balasquide DOT xyz
       </p>
     </div>
   );
